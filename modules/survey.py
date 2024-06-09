c3a4abfe3faa67a786c581cbd338f008525843b9
Sunny Thaper <sthaper@gmail.com>
2024-06-09 01:45:47 -0700

    refactor: Separate asking questions from conducting survey in Survey module

--- modules/survey.py
+++ modules/survey.py
@@ -7,17 +7,19 @@ class Survey:
         self.template = template
         self.results = self.conduct()
 
-    def conduct(self):
+    def conduct(self) -> models.Survey:
         questions = self._get_survey()
-        responses = []
-
-        for question in questions.split('\n'):
-            answer = input(f'{question}\n')
-            responses.append(models.Response(
-                question=question, answer=answer))
-
+        responses = self._ask_questions(questions)
         return models.Survey(responses=responses)
 
     def _get_survey(self) -> str:
         with open(f'{self.config.surveys_dir}/{self.template}.{self.config.surveys_extension}', 'r') as file:
             return file.read()
+
+    def _ask_questions(self, questions: str) -> list[models.Response]:
+        responses = []
+        for question in questions.split('\n'):
+            answer = input(f'{question}\n')
+            responses.append(models.Response(
+                question=question, answer=answer))
+        return responses
